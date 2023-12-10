6c3bf70afc7f048eab28e8ff30fc7b49a43c2045
Zhang,Yuheng <zhangeek@qq.com>
2023-12-10 21:06:18 +0800

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import '@nutui/nutui-react-taro/dist/style.css'
+
+import { PropsWithChildren } from 'react'
+import { useLaunch } from '@tarojs/taro'
+import './app.css'
+
+function App({ children }: PropsWithChildren<any>) {
+
+  useLaunch(() => {
+    console.log('App launched.')
+  })
+
+  // children 是将要会渲染的页面
+  return children
+}
+
+export default App

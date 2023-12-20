29ee4b695884944b38ce366617c144f0b1702cf2
Zhang,Yuheng <zhangeek@qq.com>
2023-12-20 16:44:48 +0800

    tab not scroll

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,24 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
+import {useState} from "react";
+import {Tabs} from "@nutui/nutui-react-taro";
 import './index.css'
 
 export default function Index() {
 
-  useLoad(() => {
-    console.log('Page loaded.')
-  })
-
+  const [tab4value, setTab4value] = useState('0');
   return (
-    <View className='index'>
-      <Text>Hello world!</Text>
-    </View>
+    <>
+      <Tabs
+        value={tab4value}
+        onChange={(value) => {
+          setTab4value(value as string)
+        }}
+      >
+        <Tabs.TabPane title='低阶特卖'>低阶特卖</Tabs.TabPane>
+        <Tabs.TabPane title='上新日'>上新日</Tabs.TabPane>
+        <Tabs.TabPane title='百亿补贴'>百亿补贴</Tabs.TabPane>
+        <Tabs.TabPane title='今日聚超值'>今日聚超值</Tabs.TabPane>
+        <Tabs.TabPane title='真好真便宜'>真好真便宜</Tabs.TabPane>
+      </Tabs>
+    </>
   )
 }
